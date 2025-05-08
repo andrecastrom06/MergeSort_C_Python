1351e11133a6461b8fcb1d7a7603180ce13f1642
andrecastrom06 <alcms@cesar.school>
2025-05-08 08:34:23 -0300

    codigos

---
+++ mergesortC.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void merge(int arr[], int esq, int meio, int dir) {
+    int n1 = meio - esq + 1;
+    int n2 = dir - meio;
+
+    int *L = malloc(n1 * sizeof(int));
+    int *R = malloc(n2 * sizeof(int));
+
+    for (int i = 0; i < n1; i++)
+        L[i] = arr[esq + i];
+    for (int j = 0; j < n2; j++)
+        R[j] = arr[meio + 1 + j];
+
+    int i = 0, j = 0, k = esq;
+
+    while (i < n1 && j < n2) {
+        if (L[i] <= R[j])
+            arr[k++] = L[i++];
+        else
+            arr[k++] = R[j++];
+    }
+
+    while (i < n1)
+        arr[k++] = L[i++];
+
+    while (j < n2)
+        arr[k++] = R[j++];
+
+    free(L);
+    free(R);
+}
+
+void mergeSort(int arr[], int esq, int dir) {
+    if (esq < dir) {
+        int meio = esq + (dir - esq) / 2;
+
+        mergeSort(arr, esq, meio);
+        mergeSort(arr, meio + 1, dir);
+        merge(arr, esq, meio, dir);
+    }
+}
+
+void printArray(int arr[], int tamanho) {
+    for (int i = 0; i < tamanho; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+}
+
+int main() {
+    int arr[] = {38, 27, 43, 3, 9, 82, 10};
+    int tamanho = sizeof(arr) / sizeof(arr[0]);
+
+    mergeSort(arr, 0, tamanho - 1);
+
+    printArray(arr, tamanho);
+    return 0;
+}
